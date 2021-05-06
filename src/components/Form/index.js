529fea6c7c9efd4a25cca25087f6b5e8ae156abb
Rosalinda Oliveira <rosimartinsoliveira@gmail.com>
2021-05-06 21:35:17 +0100

    add components

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { TextInput, View, Text, TouchableOpacity, Vibration} from "react-native";
+import { TextInput, View, Text, TouchableOpacity, Vibration, Pressable, Keyboard } from "react-native";
 import styles from "./style.js"
 import ResultImc from "./ResultImc/index.js";
 
@@ -13,7 +13,8 @@ export default function Form() {
     const [errorMessage, setErrorMessage] = useState(null);
 
     function imcCalculator() {
-        return setImc((weight/(height*height)).toFixed(2));
+        let heightFormat = height.replace(",",".");
+        return setImc((weight/(heightFormat*heightFormat)).toFixed(2));
     }
 
     function verificationImc() {
@@ -41,7 +42,7 @@ export default function Form() {
     }
 
     return(
-        <View style={styles.formContext}>
+        <Pressable onPress={Keyboard.dismiss} style={styles.formContext}>
 
             <View style={styles.form}>
                 <Text style={styles.formLabel}> Height </Text>
@@ -75,6 +76,6 @@ export default function Form() {
             </View>
 
             <ResultImc messageResultImc={messageImc} resultImc={imc} />
-        </View>
+        </Pressable>
     );
 }
